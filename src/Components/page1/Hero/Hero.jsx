9e544e39bf5a97a780544e735dabc0690ef29b46
ShivamBunge <shivambunge9898@gmail.com>
2022-04-11 22:48:44 +0530

    added hero section

--- src/Components/page1/Hero/Hero.jsx
+++ src/Components/page1/Hero/Hero.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
-};
+
+  return (
+    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+    <div className="carousel-inner">
+    <div className="carousel-item active">
+      <div className="row">
+      <div className="col">
+        <h1>Find Leading Experts for your Business</h1>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+      <div className="col"><img src="https://static.startuptalky.com/2021/10/Top-Consultancy-Business-Ideas_startuptalky.jpg" className="d-block w-100" alt="..."/>
+      </div>
+      </div>
+    </div>
+    <div className="carousel-item ">
+      <div className="row">
+      <div className="col">
+        <h1>Second slide label</h1>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+      <div className="col"><img src="https://static.startuptalky.com/2021/10/Top-Consultancy-Business-Ideas_startuptalky.jpg" className="d-block w-100" alt="..."/>
+      </div>
+      </div>
+    </div>
+    <div className="carousel-item active">
+      <div className="row center">
+      <div className="col ">
+      <h1>Find Leading Expert for your Business</h1>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+      <div className="col"><img src="https://static.startuptalky.com/2021/10/Top-Consultancy-Business-Ideas_startuptalky.jpg" className="d-block w-100" alt="..."/>
+      </div>
+      </div>
+    </div>
+
+    </div>
+    </div>
+    );
+      {/* https://bootsnipp.com/snippets/eK8AK */}
+    };
 
 export default Hero;
--- src/Layer.js
+++ src/Layer.js
@@ -1,13 +1,18 @@
 import React from "react";
-
+import Trending from "./Components/page1/Trending/Trending";
 import Navbar from "./Components/Global/Navbar/Navbar";
 import Hero from "./Components/page1/Hero/Hero";
+import Whyus from "./Components/page1/Whyus/Whyus";
+import Footer from "./Components/Global/Footer/Footer";
 
 const Layer = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Trending/>
+      <Whyus/>      
+      <Footer/>
     </div>
   );
 };
