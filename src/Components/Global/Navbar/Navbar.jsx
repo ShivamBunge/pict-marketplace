9d794a821deb214556b673a3ba500df8a2f67bb4
ShivamBunge <shivambunge9898@gmail.com>
2022-04-18 18:01:35 +0530

    hero section added

--- src/Components/Global/Navbar/Navbar.jsx
+++ src/Components/Global/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./navbar.css";
 function Navbar() {
   return (
-    <nav className="my-nav navbar navbar-expand-lg navbar-dark ">
+    <nav className="my-nav navbar navbar-expand-lg navbar-light fixed-top">
 <div className="container-fluid">
-<a className="navbar-brand" href="#">Navbar</a>
+<a className="navbar-brand" href="#">MentorNow</a>
 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span className="navbar-toggler-icon"></span>
 </button>
