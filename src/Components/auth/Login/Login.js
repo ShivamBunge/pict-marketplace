b4602609abcbda886b7d4cd1ab5570f398ca8c80
ShivamBunge <shivambunge9898@gmail.com>
2022-06-13 15:16:45 +0530

    email auth added

--- src/Components/auth/Login/Login.js
+++ src/Components/auth/Login/Login.js
@@ -4,13 +4,40 @@ import { signInWithRedirect,GoogleAuthProvider,getRedirectResult } from "firebas
 import { auth } from "../../../firebase";
 import styles from "./Login.module.css";
 import google from "../../../Assets/google.png";
-
+import { signInWithEmailAndPassword } from "firebase/auth";
+import InputControl from "../InputControl/InputControl";
 function Login() {
   const navigate = useNavigate();
   
   const [errorMsg, setErrorMsg] = useState("");
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+ 
+  const [values, setValues] = useState({
+    email: "",
+    pass: "",
+  });
+ 
+ 
 
+  const handleEmailSubmission = () => {
+    if (!values.email || !values.pass) {
+      setErrorMsg("Fill all fields");
+      return;
+    }
+    setErrorMsg("");
+
+    setSubmitButtonDisabled(true);
+    signInWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async (res) => {
+        setSubmitButtonDisabled(false);
+        
+        navigate("/");
+      })
+      .catch((err) => {
+        setSubmitButtonDisabled(false);
+        setErrorMsg(err.message);
+      });
+  };
   const handleSubmission = () => {
       const provider=new GoogleAuthProvider();
       signInWithRedirect(auth,provider)
@@ -33,9 +60,27 @@ function Login() {
       <div className={styles.innerBox}>
         <h1 className={styles.heading}>Login</h1>
 
-      
+          <b className={styles.error}>{errorMsg}</b>
+          <InputControl
+          label="Email"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+          placeholder="Enter email address"
+        />
+        <InputControl
+          label="Password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+          placeholder="Enter Password"
+        />
+
         <div className={styles.footer}>
           <b className={styles.error}>{errorMsg}</b>
+          <button disabled={submitButtonDisabled} onClick={handleEmailSubmission}>
+            Login
+          </button>
          
           <button className="btn btn-light" onClick={handleSubmission} disabled={submitButtonDisabled}>
             <img src={google} height="40px" width="40px"/>continue with Google</button>
@@ -45,7 +90,8 @@ function Login() {
               <Link to="/signup">Sign Up</Link>
             </span>
           </p>
-        </div>
+          </div>
+        
       </div>
     </div>
   );
--- src/Components/auth/Signup/Signup.js
+++ src/Components/auth/Signup/Signup.js
@@ -4,10 +4,40 @@ import { signInWithRedirect,GoogleAuthProvider,getRedirectResult } from "firebas
 import { auth } from "../../../firebase";
 import styles from "./Signup.module.css";
 import google from "../../../Assets/google.png";
+import InputControl from "../InputControl/InputControl";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 function Signup() {
   const navigate = useNavigate();
   const [errorMsg, setErrorMsg] = useState("");
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    pass: "",
+  });
+
+  const handleEmailSubmission = () => {
+    if (!values.name || !values.email || !values.pass) {
+      setErrorMsg("Fill all fields");
+      return;
+    }
+    setErrorMsg("");
+
+    setSubmitButtonDisabled(true);
+    createUserWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async (res) => {
+        setSubmitButtonDisabled(false);
+        const user = res.user;
+        await updateProfile(user, {
+          displayName: values.name,
+        });
+        navigate("/");
+      })
+      .catch((err) => {
+        setSubmitButtonDisabled(false);
+        setErrorMsg(err.message);
+      });
+  };
 
   const handleSubmission = () => {
       const provider=new GoogleAuthProvider();
@@ -31,9 +61,32 @@ function Signup() {
       <div className={styles.innerBox}>
         <h1 className={styles.heading}>Signup</h1>
 
-      
+        <InputControl
+          label="Name"
+          placeholder="Enter your name"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, name: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Email"
+          placeholder="Enter email address"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Password"
+          placeholder="Enter password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+        />
         <div className={styles.footer}>
-          <b className={styles.error}>{errorMsg}</b>
+        <b className={styles.error}>{errorMsg}</b>
+          <button onClick={handleEmailSubmission} disabled={submitButtonDisabled}>
+            Submit
+          </button>
          
           <button className="btn btn-light" onClick={handleSubmission} disabled={submitButtonDisabled}>
             <img src={google} height="40px" width="40px"/>continue with Google</button>
